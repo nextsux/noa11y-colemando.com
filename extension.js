1c67df11b1f6bdfd451ab3f8b5b0a9acbb8a2e36
Martin -nexus- Filip <nexus@smoula.net>
2012-11-01 21:00:11 +0100

    simplified and fixed to work with gnome 3.6

--- extension.js
+++ extension.js
@@ -2,19 +2,15 @@ const Panel = imports.ui.panel;
 const Main = imports.ui.main;
 
 function enable() {
-	for (let i = 0; i < Main.panel._rightBox.get_children().length; i++) {
-		if (Main.panel._statusArea['a11y'] == Main.panel._rightBox.get_children()[i]._delegate) {
-			Main.panel._rightBox.get_children()[i].destroy();
-			break;
-		} 
-	}
-	// addToStatusArea would throw an error on disable if we don't set this to null 
-	Main.panel._statusArea['a11y'] = null;
+  if (Main.panel.statusArea['a11y']) {
+    Main.panel.statusArea['a11y'].actor.hide();
+  } 
 }
 
 function disable() {
-	let indicator = new Panel.STANDARD_STATUS_AREA_SHELL_IMPLEMENTATION["a11y"];
-	Main.panel.addToStatusArea("a11y", indicator, Panel.STANDARD_STATUS_AREA_ORDER.indexOf('a11y'));
+  if (Main.panel.statusArea['a11y']) {
+    Main.panel.statusArea['a11y'].actor.show();
+  } 
 }
 
 function init() {
